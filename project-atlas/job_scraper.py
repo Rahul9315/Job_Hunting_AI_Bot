3c6a7dbf05ba9f8764cc9ffd9102ff4414055288
Rahul <baghel931515@gmail.com>
2026-01-19 02:09:29 +0000

    changes made for jobs only for easy apply

--- project-atlas/job_scraper.py
+++ project-atlas/job_scraper.py
@@ -60,6 +60,7 @@ def load_context(browser):
 
 def scrape_linkedin(page, query):
     url = f"https://www.linkedin.com/jobs/search/?keywords={query.replace(' ', '%20')}&location=Ireland"
+    #url = f"https://www.linkedin.com/jobs/search/?keywords={query.replace(' ', '%20')}&location=Ireland&f_AL=true" # easy apply jobs only link
     page.goto(url)
     page.wait_for_timeout(8000)
 
